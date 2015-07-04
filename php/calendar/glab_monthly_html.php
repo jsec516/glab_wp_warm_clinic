3f81bf7c68e380569061939cb8b5decf198f5f87
jahidul islam <jsec516@gmail.com>
2015-07-04 10:07:11 +0600

    monthly calendar refactored

--- php/calendar/glab_monthly_html.php
+++ php/calendar/glab_monthly_html.php
@@ -73,26 +73,41 @@ class glab_monthly_html
 									<img id="<?= $previous_month[0] . '-' . $previous_month[1]; ?>" src="<?php echo plugins_url('glab_clinic/assets/images/nav-left.png'); ?>" alt="" />								</a>							</span> 
 							<span id="curr_month_year" style="padding-left: 20px; padding-right: 20px; vertical-align: top;">
 								<?= $recent_month_in_text[0] . ' ' . $recent_month_in_text[1]; ?>
-							</span>							<span class="cal_right_cursor"> <a class="next-number"						data-view_type="monthly"						data-month="<?php echo $next_month[0]; ?>"						data-year="<?php echo $next_month[1]; ?>"> <img							id="<?= $next_month[0] . '-' . $next_month[1]; ?>"							src="<?php echo plugins_url('glab_clinic/assets/images/nav-right.png'); ?>"							alt="" />					</a>					</span>				</div>				<div id="week_opt" style="display: none; float: right;">					<span class="cal_left_cursor"> <a class="prev-number"						data-view_type="weekly"						data-month="<?php echo $previous_month[0]; ?>"						data-year="<?php echo $previous_month[1]; ?>"						href="<?php echo $_SERVER['PHP_SELF'] . '?page=gc_service_schedule&month=' . $previous_month[0] . '&year=' . $previous_month[1]; ?>">							<img id="<?= $previous_month[0] . '-' . $previous_month[1]; ?>"							src="<?php echo plugins_url('glab_clinic/assets/images/nav-left.png'); ?>"							alt="" />					</a>					</span> <span id="week_opt_number"						style="padding-left: 20px; padding-right: 20px; vertical-align: top;"><?= $recent_month[0] . ' ' . $recent_month[1]; ?></span>					<span class="cal_right_cursor"> <a class="next-number"						data-view_type="weekly" data-month="<?php echo $next_month[0]; ?>"						data-year="<?php echo $next_month[1]; ?>"> <img							id="<?= $next_month[0] . '-' . $next_month[1]; ?>"							src="<?php echo plugins_url('glab_clinic/assets/images/nav-right.png'); ?>"							alt="" />					</a>					</span>				</div>				<div id="daily_opt" style="display: none; float: right;">					<span class="cal_left_cursor"> <a class="prev-number"						data-view_type="daily"						data-month="<?php echo $previous_month[0]; ?>"						data-year="<?php echo $previous_month[1]; ?>"> <img							id="<?= $previous_month[0] . '-' . $previous_month[1]; ?>"							src="<?php echo plugins_url('glab_clinic/assets/images/nav-left.png'); ?>"							alt="" />					</a>					</span> <span id="daily_opt_number"						style="padding-left: 20px; padding-right: 20px; vertical-align: top;"><?= $recent_month[0] . ' ' . $recent_month[1]; ?></span>					<span class="cal_right_cursor"> <a class="next-number"						data-view_type="daily" data-month="<?php echo $next_month[0]; ?>"						data-year="<?php echo $next_month[1]; ?>"> <img							id="<?= $next_month[0] . '-' . $next_month[1]; ?>"							src="<?php echo plugins_url('glab_clinic/assets/images/nav-right.png'); ?>"							alt="" />					</a>					</span>				</div>			</td>		</tr>	</table>	<input type="hidden" id="view_type" value="<?= $view_type; ?>" /></div><?php
+							</span>							<span class="cal_right_cursor"> 
+								<a class="next-number" data-view_type="monthly" data-month="<?php echo $next_month[0]; ?>" data-year="<?php echo $next_month[1]; ?>"> 
+									<img id="<?= $next_month[0] . '-' . $next_month[1]; ?>" src="<?php echo plugins_url('glab_clinic/assets/images/nav-right.png'); ?>" alt="" />								</a>							</span>						</div>						<div id="week_opt" style="display: none; float: right;">							<span class="cal_left_cursor"> 
+								<a class="prev-number" data-view_type="weekly" data-month="<?php echo $previous_month[0]; ?>" data-year="<?php echo $previous_month[1]; ?>" href="<?php echo $_SERVER['PHP_SELF'] . '?page=gc_service_schedule&month=' . $previous_month[0] . '&year=' . $previous_month[1]; ?>">									<img id="<?= $previous_month[0] . '-' . $previous_month[1]; ?>" src="<?php echo plugins_url('glab_clinic/assets/images/nav-left.png'); ?>" alt="" />								</a>							</span> 
+							<span id="week_opt_number" style="padding-left: 20px; padding-right: 20px; vertical-align: top;">
+								<?= $recent_month[0] . ' ' . $recent_month[1]; ?>
+							</span>							<span class="cal_right_cursor"> 
+								<a class="next-number" data-view_type="weekly" data-month="<?php echo $next_month[0]; ?>" data-year="<?php echo $next_month[1]; ?>"> 
+									<img id="<?= $next_month[0] . '-' . $next_month[1]; ?>" src="<?php echo plugins_url('glab_clinic/assets/images/nav-right.png'); ?>" alt="" />								</a>							</span>						</div>						<div id="daily_opt" style="display: none; float: right;">							<span class="cal_left_cursor"> 
+								<a class="prev-number" data-view_type="daily" data-month="<?php echo $previous_month[0]; ?>" data-year="<?php echo $previous_month[1]; ?>"> 
+									<img id="<?= $previous_month[0] . '-' . $previous_month[1]; ?>" src="<?php echo plugins_url('glab_clinic/assets/images/nav-left.png'); ?>" alt="" />								</a>							</span> 
+							<span id="daily_opt_number" style="padding-left: 20px; padding-right: 20px; vertical-align: top;">
+							    <?= $recent_month[0] . ' ' . $recent_month[1]; ?>
+							</span>							<span class="cal_right_cursor"> 
+								<a class="next-number" data-view_type="daily" data-month="<?php echo $next_month[0]; ?>" data-year="<?php echo $next_month[1]; ?>"> 
+									<img id="<?= $next_month[0] . '-' . $next_month[1]; ?>" src="<?php echo plugins_url('glab_clinic/assets/images/nav-right.png'); ?>" alt="" />								</a>							</span>						</div>					</td>				</tr>			</table>			<input type="hidden" id="view_type" value="<?= $view_type; ?>" />		</div>	<?php
 	}
-	static function draw_calendar($args) {
+	
+	static function draw_calendar($args) 
+	{
 		extract ( $args );
-		/* draw table */
+		// draw table
 		$calendar = '<table cellpadding="0" cellspacing="0" class="calendar b_common">';
-		/* table headings */
-		
+		// table headings
 		$headings = array (
-				'Mon',
-				'Tue',
-				'Wed',
-				'Thu',
-				'Fri',
-				'Sat',
-				'Sun' 
+			'Mon',
+			'Tue',
+			'Wed',
+			'Thu',
+			'Fri',
+			'Sat',
+			'Sun' 
 		);
 		$calendar .= '<tr class="calendar-row"><td class="calendar_week_head" style="width:50px;">Week</td><td class="calendar-day-head">' . implode ( '</td><td class="calendar-day-head">', $headings ) . '</td></tr>';
-		/* days and weeks vars now ... */
-		
+		// days and weeks vars now ...
 		$running_day = date ( 'N', mktime ( 0, 0, 0, $month, 1, $year ) ); // mdified_ w
 		$days_in_month = date ( 't', mktime ( 0, 0, 0, $month, 1, $year ) );
 		$days_in_this_week = 1;
@@ -100,32 +115,29 @@ class glab_monthly_html
 		$dates_array = array ();
 		$prev_month_day = date ( 't', mktime ( 0, 0, 0, $month - 1, 1, $year ) );
 		$prev_day_print = $prev_month_day + 1 - $running_day;
-		
-		/* row for week one */
-		$calendar .= '<tr class="calendar-row">';
-		/* print "blank" days until the first of the current week */
-		
-		$recent_date_ = $year . '-' . $month . '-01';
+		$calendar .= '<tr class="calendar-row">'; // row for week one
+		$recent_date_ = $year . '-' . $month . '-01'; // print "blank" days until the first of the current week
 		$recent_week_date_ = $year . '-' . $month . '-' . date ( 'd' );
-		
 		$calendar .= '<td class="calendar_week" style=""><input type="hidden" id="day" value="' . date ( 'd', strtotime ( $recent_week_date_ ) ) . '"  /><input type="hidden" id="week_num" value="' . date ( 'W', strtotime ( $recent_week_date_ ) ) . '" /><input type="hidden" id="month" value="' . date ( 'n', strtotime ( $recent_date_ ) ) . '" /><input type="hidden" id="dailyLabel" value="' . date ( 'd-F', strtotime ( $recent_week_date_ ) ) . '"/>			<input type="hidden" id="year" value="' . date ( 'Y', strtotime ( $recent_date_ ) ) . '" /><a href="" style="color:#fff;font-weight:bold;">' . date ( 'W', strtotime ( $recent_date_ ) ) . '</a></td>';
 		
-		for($x = 1; $x < $running_day; $x ++) : // ordignal 0
+		for ($x = 1; $x < $running_day; $x ++) { // ordignal 0
 			$calendar .= '<td class="calendar-day-np">' . ++ $prev_day_print . '</td>';
 			$days_in_this_week ++;
-		endfor
-		;
+		}
 		
-		for($list_day = 1; $list_day <= $days_in_month; $list_day ++) :
+		for ($list_day = 1; $list_day <= $days_in_month; $list_day ++) {
 			$selectedHDate = $year . '-' . $month . '-' . $list_day;
 			$closedStatus = 0;
 			$calendar .= '<td id="' . $list_day . '" class="calendar-day" style="min-width:130px!important;overflow:hidden;">';
 			$selectedHDate = $year . '-' . str_pad ( $month, 2, '0', STR_PAD_LEFT ) . '-' . str_pad ( $list_day, 2, '0', STR_PAD_LEFT );
 			$selector_date_format = $year . '-' . str_pad ( $month, 2, '0', STR_PAD_LEFT ) . '-' . str_pad ( $list_day, 2, '0', STR_PAD_LEFT );
-			if (isset ( $appointments [$selector_date_format] ))
+			
+			if (isset ( $appointments [$selector_date_format] )) {
 				$dailyAppt = count ( $appointments [$selector_date_format] );
-			else
+			} else {
 				$dailyAppt = 0;
+			}
+			
 			if ($dailyAppt > 0) {
 				$calendar .= '<div class="day-number" style="font-weight:bold;">' . $list_day;
 				$calendar .= '</div>';
@@ -136,7 +148,6 @@ class glab_monthly_html
 				$calendar .= '<div class="day-number">' . $list_day;
 				
 				if ((in_array ( $dayChar, $regular_off_days ) != false) or (in_array ( $selectedHDate, $monthly_off_days ) != false)) {
-					
 					$calendar .= '<div style="position:relative; width:80%; margin:auto; color:#FF0000;  font-weight:bold; text-align:center;  height:auto; Font-size:12px; border:0px solid black; clear:both; ">Closed</div>';
 				}
 				
@@ -145,57 +156,36 @@ class glab_monthly_html
 			
 			$calendar .= '</td>';
 			
-			if ($running_day == 7) : // original 6
-				
+			if ($running_day == 7) { // original 6
 				$calendar .= '</tr>';
 				
-				if (($day_counter + 1) != $days_in_month) :
-					
+				if (($day_counter + 1) != $days_in_month) {
 					$calendar .= '<tr class="calendar-row">';
-				
-                endif;
+				}
 				
 				$running_day = 0; // original -1
-				
 				$days_in_this_week = 0;
-				
 				$next_day_ = $list_day + 1; // original +2
-				
 				$recent_date_ = $year . '-' . $month . '-' . $next_day_;
-				
 				$calendar .= '<td class="calendar_week"><a href="" style="color:#fff;font-weight:bold;">' . date ( 'W', strtotime ( $recent_date_ ) ) . '</a></td>';
-			
-            endif;
+			}
 			
 			$days_in_this_week ++;
-			
 			$running_day ++;
-			
 			$day_counter ++;
-		endfor
-		;
-		
-		/* finish the rest of the days in the week */
-		
-		if ($days_in_this_week < 8) {
-			
-			for($x = 1; $x <= (8 - $days_in_this_week); $x ++) :
-				
-				$calendar .= '<td class="calendar-day-np">' . $x . '</td>';
-			endfor
-			;
 		}
 		
-		/* final row */
-		
+		// finish the rest of the days in the week 
+		if ($days_in_this_week < 8) {
+			for($x = 1; $x <= (8 - $days_in_this_week); $x ++) {
+				$calendar .= '<td class="calendar-day-np">' . $x . '</td>';
+			}
+		}
+		// final row
 		$calendar .= '</tr>';
-		
-		/* end the table */
-		
+		// end the table
 		$calendar .= '</table>';
-		
-		/* all done, return result */
-		
+		// all done, return result 
 		return $calendar;
 	}
 }
